aa25f455285d85a38c0acde550ad6d2ea3148025
Vini_L <142224279+vin1ciusL@users.noreply.github.com>
2026-04-21 00:43:04 -0300

    bug do menu arrumado

--- unitycode/my8way/Assets/MenuActions.cs
+++ unitycode/my8way/Assets/MenuActions.cs
@@ -6,11 +6,11 @@ public class ButtonScript : MonoBehaviour
     public void IniciaJogo()
     {
         GameController.Init();
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1); // Vai para o jogo (Cena 1)
     }
 
     public void Menu()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0); // Volta para o Menu (Cena 0)
     }
 }
